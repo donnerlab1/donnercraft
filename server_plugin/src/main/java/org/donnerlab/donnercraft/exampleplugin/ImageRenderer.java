d00517d5c37ba90812c0d25ef51b17fd8af70bdd
timlueg <7452348+timlueg@users.noreply.github.com>
2018-07-02 23:08:18 +0200

    QR code smaller for better detection and only render once

--- server_plugin/src/main/java/org/donnerlab/donnercraft/exampleplugin/ImageRenderer.java
+++ server_plugin/src/main/java/org/donnerlab/donnercraft/exampleplugin/ImageRenderer.java
@@ -4,18 +4,25 @@ import org.bukkit.entity.Player;
 import org.bukkit.map.MapCanvas;
 import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
+import org.bukkit.map.MinecraftFont;
 
 import java.awt.Image;
 
 public class ImageRenderer extends MapRenderer {
 
     private Image image;
+    private boolean hasRendered = false;
     @Override
     public void render(MapView map, MapCanvas canvas, Player player) {
-        canvas.drawImage(0,0, image);
+    	if (hasRendered)
+    		return;
+
+    	canvas.drawText(0, 0, MinecraftFont.Font, "Scan this Invoice to Pay");
+        canvas.drawImage(0,8, image);
+        hasRendered = true;
     }
 
-    public ImageRenderer(Image image) {
+    ImageRenderer(Image image) {
         this.image = image;
     }
 }
--- server_plugin/src/main/java/org/donnerlab/donnercraft/exampleplugin/QRMapSpawner.java
+++ server_plugin/src/main/java/org/donnerlab/donnercraft/exampleplugin/QRMapSpawner.java
@@ -2,12 +2,9 @@ package org.donnerlab.donnercraft.exampleplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.command.CommandMap;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 import com.google.zxing.BarcodeFormat;
@@ -18,24 +15,25 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import org.bukkit.map.MapView;
 
 
-public class QRMapSpawner {
+class QRMapSpawner {
 
-    public static void SpawnMap(Player p, String content) {
+    static void SpawnMap(Player p, String content) {
         ItemStack itemStack = new ItemStack(Material.MAP, 1);
         MapView view = Bukkit.createMap(p.getWorld());
         view.getRenderers().clear();
         view.addRenderer(new ImageRenderer(generateQRCode(content)));
         itemStack.setDurability(view.getId());
         p.getInventory().addItem(itemStack);
-        
     }
 
     private static BufferedImage generateQRCode(String content) {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         try {
-            BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, 128, 128);
+            BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, 128, 120);
             return MatrixToImageWriter.toBufferedImage(bitMatrix);
-        } catch (WriterException e){}
+        } catch (WriterException e){
+        	e.printStackTrace();
+		}
         return null;
     }
 
